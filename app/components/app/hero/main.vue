51c0c4605a53e039784d3002b8637919d2046064
YeiyoNathnael <138720671+YeiyoNathnael@users.noreply.github.com>
2026-01-05 23:57:30 +0400

    feat: Add Main hero page

---
+++ app/components/app/hero/main.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const links = ref([
+  {
+    label: 'Join Club Now',
+  },
+  {
+    label: 'Learn more',
+    color: 'neutral',
+    variant: 'subtle',
+    trailingIcon: 'i-lucide-arrow-right'
+  }
+])
+</script>
+
+<template>
+  <UPageHero
+    headline="Premium Chess Club Experience"
+    title="Master Chess With ETHCHESS Club"
+    description="Join our elite chess community. Compete in tournaments, climb leaderboards and connect with chess enthusiasts from around the world"
+    :links="links"
+  />
+</template>
